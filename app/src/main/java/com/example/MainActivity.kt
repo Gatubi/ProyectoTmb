50b8a63449b92b8e1b13c7e1968598eb492d285a
Gatubi <alexignacio.medina@gmail.com>
2023-10-23 21:35:06 -0300

    GRANDES AVANCES PARA LA HUMANIDAD

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.proyectotmb.ActivityImc
+import com.example.proyectotmb.ActivityTmb
 import com.example.proyectotmb.R
 
 class MainActivity : AppCompatActivity() {
@@ -17,18 +21,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun initComponent(){
-
+        btnImc = findViewById(R.id.btnImc)
+        btnTmb = findViewById(R.id.btnTmb)
     }
 
     fun initListener(){
-
+        btnImc.setOnClickListener { toImc() }
+        btnTmb.setOnClickListener { toTmb() }
     }
 
     private fun toImc(){
-
+        val intent = Intent(this, ActivityImc::class.java)
+        startActivity(intent)
     }
 
     private fun toTmb(){
-
+        val intent = Intent(this, ActivityTmb::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/proyectotmb/ActivityImc.kt
@@ -0,0 +1,11 @@
+package com.example.proyectotmb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityImc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
