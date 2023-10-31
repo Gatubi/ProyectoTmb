ffb45388373ab3f13bd437fe52052729b97aef7c
Gatubi <alexignacio.medina@gmail.com>
2023-10-30 21:32:06 -0300

    Creacion de Result para TMB

---
+++ app/src/main/java/com/example/proyectotmb/tmb/ResultTmbActivity.kt
@@ -0,0 +1,12 @@
+package com.example.proyectotmb.tmb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyectotmb.R
+
+class ResultTmbActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_tmb)
+    }
+}
